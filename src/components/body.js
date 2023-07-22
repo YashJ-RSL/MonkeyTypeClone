9c1264f086498ecb23ae656c6ff66ecbb7df4896
yash jain <yash.jain@rajasoftwarelabs.com>
2023-07-22 18:30:36 +0530

    Added space feature

--- src/components/body.js
+++ src/components/body.js
@@ -37,25 +37,44 @@ const Body = () => {
           if (inputText === "" && !isTimerRunning) {
             setIsTimerRunning(true);
           }
-          let temp = inputText + event.key;
-          setInputText(temp);
-          setCounter(counter + 1);
           const char = document.getElementById(
             `span-char-${counter}`
           ).innerHTML;
-          if (char === event.key) {
-            document.getElementById(`span-char-${counter}`).style.color =
-              "white";
-          } else {
-            document.getElementById(`span-char-${counter}`).style.color = "red";
-          }
-          if (counter > 0) {
+          if (event.key === " " && char !== " ") {
+            let counterTemp = counter;
+            let inputTextTemp = inputText;
             document.getElementById(
               `span-char-${counter - 1}`
             ).style.borderRight = "None";
+            while (sentences[counterTemp] !== " ") {
+              counterTemp++;
+              inputTextTemp += "/";
+            }
+            setInputText(inputTextTemp);
+            setCounter(counterTemp + 1);
+            document.getElementById(
+              `span-char-${counterTemp}`
+            ).style.borderRight = "1px solid white";
+          } else {
+            let temp = inputText + event.key;
+            setInputText(temp);
+            setCounter(counter + 1);
+            if (char === event.key) {
+              document.getElementById(`span-char-${counter}`).style.color =
+                "white";
+            } else {
+              document.getElementById(`span-char-${counter}`).style.color =
+                "red";
+            }
+            if (counter > 0) {
+              document.getElementById(
+                `span-char-${counter - 1}`
+              ).style.borderRight = "None";
+            }
+            console.log(counter);
+            document.getElementById(`span-char-${counter}`).style.borderRight =
+              "1px solid white";
           }
-          document.getElementById(`span-char-${counter}`).style.borderRight =
-            "1px solid white";
           document.getElementById("timer").style.opacity = "1";
           document.getElementById("nav").style.display = "none";
         }
